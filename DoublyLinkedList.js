175984397b8be9da79e613b24e21cde8653e23b6
dipankarhalder <webmail.dip@gmail.com>
2022-06-08 10:58:04 +0530

    doubly linked list

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -108,6 +108,107 @@ class DoublyLinkedList {
     this.getNodeAt(index).value = value;
     return this;
   }
+
+  // insert value at index
+  insert(index, value) {
+    // if index is negative or greater than length, return undefined
+    if (index < 0 || index >= this.length) return undefined;
+
+    // if index is 0, set head to new node
+    if (index === 0) {
+      this.unshift(value);
+    } else if (index === this.length) {
+      // if index is length, set tail to new node
+      this.push(value);
+    } else {
+      // otherwise, set current to head and iterate through list until index is reached
+      let newNode = new Node(value);
+      let current = this.head;
+      for (let i = 1; i <= index; i++) {
+        current = current.next;
+      }
+      newNode.next = current.next;
+      current.next = newNode;
+      this.length++;
+    }
+    return this;
+  }
+
+  // remove value at index
+  remove(index) {
+    // if index is negative or greater than length, return undefined
+    if (index < 0 || index >= this.length) return undefined;
+
+    // if index is 0, set head to next node
+    if (index === 0) {
+      this.shift();
+    } else if (index === this.length - 1) {
+      // if index is length - 1, set tail to previous node
+      this.pop();
+    } else {
+      // otherwise, set current to head and iterate through list until index is reached
+      let current = this.head;
+      for (let i = 1; i < index; i++) {
+        current = current.next;
+      }
+      current.next = current.next.next;
+      this.length--;
+    }
+    return this;
+  }
+
+  // reverse list
+  reverse() {
+    // if list is empty, return undefined
+    if (!this.head) return undefined;
+
+    // set current to head and previous to null
+    let current = this.head;
+    let previous = null;
+    // iterate through list
+    while (current) {
+      // set next to current's next
+      let next = current.next;
+      // set current's next to previous
+      current.next = previous;
+      // set previous to current
+      previous = current;
+      // set current to next
+      current = next;
+    }
+    // set head to previous
+    this.head = previous;
+    return this;
+  }
+
+  // print list
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+
+  // print list in reverse
+  printReverse() {
+    let current = this.tail;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+
+  // return list as array
+  toArray() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+    return arr;
+  }
 }
 
 const list = new DoublyLinkedList();
